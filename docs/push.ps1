71c65199ec4639f9b8dad4ecaae101c7168b9eb4
stax76 <frank_skare@yahoo.de>
2017-10-02 18:43:48 +0200

    -

--- docs/push.ps1
+++ docs/push.ps1
@@ -1,5 +1,6 @@
 . C:\Users\Frank\AppData\Local\GitHub\shell.ps1
 git commit -m "-" -- D:\Projekte\VS\VB\StaxRip\docs
+git commit -m "-" -- D:\Projekte\VS\VB\StaxRip\Screenshots
 git commit -m "-" -- D:\Projekte\VS\VB\StaxRip\README.md
 git commit -m "-" -- D:\Projekte\VS\VB\StaxRip\changelog.md
 git push

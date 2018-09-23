d2fb73b4ec34f0232be3494a078977f329a7db64
Revan654 <38091898+Revan654@users.noreply.github.com>
2018-09-23 11:52:59 -0400

    Add files via upload

--- My Project/AssemblyInfo.vb
+++ My Project/AssemblyInfo.vb
@@ -4,8 +4,9 @@ Imports System.Runtime.InteropServices
 <Assembly: AssemblyTitle("StaxRip")>
 <Assembly: AssemblyCompany("")>
 <Assembly: AssemblyProduct("StaxRip")>
-<Assembly: AssemblyCopyright("stax76")>
+<Assembly: AssemblyCopyright("Revan654")>
 <Assembly: ComVisible(False)> 
 <Assembly: Guid("54d5dc0f-6ba7-42f0-a82f-9c71d561cb8b")>
 <Assembly: AssemblyVersion("1.9.0.0")>
 <Assembly: AssemblyFileVersion("1.9.0.0")>
+<Assembly: AssemblyDescription("Thanks to Stax76 for Making Orig. StaxRip.")>

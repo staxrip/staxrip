ab1eab3feeb81b597b00c6e8f298d87e47c919ab
Revan654 <38091898+Revan654@users.noreply.github.com>
2019-01-17 20:14:46 -0500

    2.0

--- My Project/AssemblyInfo.vb
+++ My Project/AssemblyInfo.vb
@@ -7,5 +7,5 @@ Imports System.Runtime.InteropServices
 <Assembly: AssemblyCopyright("Revan654")>
 <Assembly: ComVisible(False)> 
 <Assembly: Guid("54d5dc0f-6ba7-42f0-a82f-9c71d561cb8b")>
-<Assembly: AssemblyVersion("2.0.0.0")>
-<Assembly: AssemblyFileVersion("2.0.0.0")>
+<Assembly: AssemblyVersion("2.0.0.1")>
+<Assembly: AssemblyFileVersion("2.0.0.1")>

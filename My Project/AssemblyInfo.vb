d8846693516a17ed570c45f6ece45e209aa443bb
Frank Skare <frank.skare.de@gmail.com>
2019-07-08 20:00:35 +0200

    -

--- My Project/AssemblyInfo.vb
+++ My Project/AssemblyInfo.vb
@@ -2,10 +2,11 @@ Imports System.Reflection
 Imports System.Runtime.InteropServices
 
 <Assembly: AssemblyTitle("StaxRip")>
-<Assembly: AssemblyCompany("")>
+<Assembly: AssemblyCompany("StaxRip authors")>
 <Assembly: AssemblyProduct("StaxRip")>
 <Assembly: AssemblyCopyright("2002-2019 StaxRip authors")>
 <Assembly: ComVisible(False)> 
 <Assembly: Guid("54d5dc0f-6ba7-42f0-a82f-9c71d561cb8b")>
-<Assembly: AssemblyVersion("2.0.2.3")>
-<Assembly: AssemblyFileVersion("2.0.2.3")>
+<Assembly: AssemblyVersion("2.0.2.5")>
+<Assembly: AssemblyFileVersion("2.0.2.5")>
+<Assembly: AssemblyDescription("Video encoding app")>

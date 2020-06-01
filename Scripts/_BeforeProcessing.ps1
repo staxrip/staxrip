670d00cc7f82ef5a83730e5fdba1446cd561b5b8
Frank Skare <frank.skare.de@gmail.com>
2020-06-01 14:59:21 +0200

    script documentation

--- Scripts/_BeforeProcessing.ps1
+++ Scripts/_BeforeProcessing.ps1
@@ -1,4 +1,7 @@
 
+# If the audio source file extension is flac and the
+# audio encoder is qaac it sets decoding mode to Pipe.
+
 $activeProject = [ShortcutModule]::p
 
 foreach ($audio in $activeProject.Audio0, $activeProject.Audio1)

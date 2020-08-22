771f2542662a8e019d061a427ab9c5b7a8548d1b
Frank Skare <frank.skare.de@gmail.com>
2020-08-22 17:39:38 +0200

    fix x265 Decoder ffmpeg DXVA2 (#290)

--- Encoding/x265Enc.vb
+++ Encoding/x265Enc.vb
@@ -1203,7 +1203,13 @@ Public Class x265Params
                     sb.Append(Package.ffmpeg.Path.Escape + " -threads 1 -hwaccel qsv -i " + p.SourceFile.Escape + " -f yuv4mpegpipe -strict -1" + crop + " -loglevel fatal -hide_banner - | " + Package.x265.Path.Escape)
                 Case "ffdxva"
                     Dim crop = If(isCropped, $" -vf ""crop={p.SourceWidth - p.CropLeft - p.CropRight}:{p.SourceHeight - p.CropTop - p.CropBottom}:{p.CropLeft}:{p.CropTop}""", "")
-                    sb.Append(Package.ffmpeg.Path.Escape + " -threads 1 -hwaccel dxva2 -i " + p.SourceFile.Escape + " -f yuv4mpegpipe -strict -1" + crop + " -loglevel fatal -hide_banner - | " + Package.x265.Path.Escape)
+                    Dim pix_fmt = "yuv420p"
+
+                    If p.BitDepth = 10 Then
+                        pix_fmt += "10le"
+                    End If
+
+                    sb.Append(Package.ffmpeg.Path.Escape + " -threads 1 -hwaccel dxva2 -i " + p.SourceFile.Escape + " -f yuv4mpegpipe -pix_fmt " + pix_fmt + " -strict -1" + crop + " -loglevel fatal -hide_banner - | " + Package.x265.Path.Escape)
             End Select
         End If
 

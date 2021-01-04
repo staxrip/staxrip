8d8b0dc2befb5e6188ea030cc44620f9d7e43279
Frank Skare <frank.skare.de@gmail.com>
2021-01-04 20:07:40 +0100

    improved error message

--- Forms/AppsForm.vb
+++ Forms/AppsForm.vb
@@ -811,7 +811,9 @@ Public Class AppsForm
                     dialog.FileName.ToLowerEx.StartsWithEx(Folder.Startup.ToLowerEx) AndAlso
                     Not dialog.FileName.ToLowerEx.StartsWithEx(Folder.Settings.ToLowerEx) Then
 
-                    MsgError("Custom paths within the startup folder are not permitted.")
+                    MsgError("Custom paths within the startup folder are not permitted " +
+                             "because it would prevent a simple update process. " +
+                             "Please put the file somewhere else outside the startup folder.")
                     Exit Sub
                 End If
 

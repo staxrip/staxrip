301920203aca7f5b0ef8389ba5a617dcaab0fa2f
Frank Skare <frank.skare.de@gmail.com>
2021-01-08 21:39:54 +0100

    #427 fix hdr ingest regression

--- General/Extras.vb
+++ General/Extras.vb
@@ -201,7 +201,7 @@ Public Class MKVInfo
                 Proc.Arguments = "-o " + (inputFile + "_HDR10.mkv").Escape + " --colour-matrix 0:9 " +
                     "--colour-range 0:1 --colour-transfer-characteristics 0:16 --colour-primaries 0:9 " +
                     "--max-content-light 0:1000 --max-frame-light 0:300 --max-luminance 0:1000 " +
-                    "--min-luminance 0:0.01 --chromaticity-coordinates 0:0.68,0.32,0.265,0.690,0.15,0.06" +
+                    "--min-luminance 0:0.01 --chromaticity-coordinates 0:0.68,0.32,0.265,0.690,0.15,0.06 " +
                     "--white-colour-coordinates 0:0.3127,0.3290 " + inputFile.Escape
                 Proc.Start()
             End Using

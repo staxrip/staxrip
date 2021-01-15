c73b7000183331c391473c027b26c12260a72d72
Dendraspis <dendraspis@gmx.net>
2021-01-15 12:44:40 +0100

    Typo fix

--- Controls/x265Control.vb
+++ Controls/x265Control.vb
@@ -111,17 +111,17 @@ Public Class x265Control
         components = New ComponentModel.Container()
 
         QualityDefinitions = New List(Of QualityItem) From {
-            New QualityItem(10, "Super High", "Super high quality and file size)"),
-            New QualityItem(12, "Very High", "Very high quality and file size)"),
-            New QualityItem(14, "Higher", "Higher quality and file size)"),
-            New QualityItem(16, "High", "High quality and file size)"),
-            New QualityItem(18, "Medium", "Medium quality and file size)"),
-            New QualityItem(20, "Low", "Low quality and file size)"),
-            New QualityItem(22, "Lower", "Lower quality and file size)"),
-            New QualityItem(24, "Very Low", "Very low quality and file size)"),
-            New QualityItem(26, "Super Low", "Super low quality and file size)"),
-            New QualityItem(28, "Extreme Low", "Extreme low quality and file size)"),
-            New QualityItem(30, "Ultra Low", "Ultra low quality and file size)")}
+            New QualityItem(10, "Super High", "Super high quality and file size"),
+            New QualityItem(12, "Very High", "Very high quality and file size"),
+            New QualityItem(14, "Higher", "Higher quality and file size"),
+            New QualityItem(16, "High", "High quality and file size"),
+            New QualityItem(18, "Medium", "Medium quality and file size"),
+            New QualityItem(20, "Low", "Low quality and file size"),
+            New QualityItem(22, "Lower", "Lower quality and file size"),
+            New QualityItem(24, "Very Low", "Very low quality and file size"),
+            New QualityItem(26, "Super Low", "Super low quality and file size"),
+            New QualityItem(28, "Extreme Low", "Extreme low quality and file size"),
+            New QualityItem(30, "Ultra Low", "Ultra low quality and file size")}
 
         Encoder = enc
         Params = Encoder.Params

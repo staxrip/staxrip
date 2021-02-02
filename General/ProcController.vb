68d587c28d6057fbb922a4fe0472e80fa526f840
Dendraspis <dendraspis@gmx.net>
2021-02-03 00:37:15 +0100

    Adjust customized progress text for x265 3.4+70

--- General/ProcController.vb
+++ General/ProcController.vb
@@ -115,10 +115,10 @@ Public Class ProcController
                     End If
                 End If
             ElseIf Proc.Package Is Package.x265 Then
-                Dim pattern = "\[((\d+)\.?(\d*))%\]\s+((\d+)/(\d+)(\sframes)),\s((\d+)\.?(\d*)(\sfps)),\s((\d+)\.?(\d*)\s([a-z]{2}/s)),\selapsed:\s(\d+:\d+:\d+),\seta:\s(\d+:\d+:\d+),\ssize:\s(\d+)\.(\d+)\s([a-z]{1,2}),\sest\.\ssize:\s(\d+)\.(\d+)\s([a-z]{1,2})"
+                Dim pattern = "\[((\d+)\.?(\d*))%\]\s+((\d+)(\(\d+\))?/(\d+)(\sframes)),\s((\d+)\.?(\d*)(\sfps)),\s((\d+)\.?(\d*)\s([a-z]{2}/s)),\selapsed:\s(\d+:\d+:\d+),\seta:\s(\d+:\d+:\d+),\ssize:\s(\d+)\.(\d+)\s([a-z]{1,2}),\sest\.\ssize:\s(\d+)\.(\d+)\s([a-z]{1,2})"
                 Dim match = Regex.Match(value, pattern, RegexOptions.IgnoreCase)
                 If match.Success Then
-                    value = $"[{match.Groups(2).Value,2}.{match.Groups(3).Value}%] {match.Groups(5).Value.PadLeft(match.Groups(6).Value.Length)}/{match.Groups(6).Value} frames @ {match.Groups(9).Value}.{match.Groups(10).Value} fps, {match.Groups(13).Value,4}.{match.Groups(14).Value} {match.Groups(15).Value}, {match.Groups(18).Value}.{match.Groups(19).Value} {match.Groups(20).Value} ({match.Groups(21).Value} {match.Groups(23).Value}), {match.Groups(16).Value} (-{match.Groups(17).Value})"
+                    value = $"[{match.Groups(2).Value,2}.{match.Groups(3).Value}%] {match.Groups(5).Value.PadLeft(match.Groups(7).Value.Length)}{match.Groups(6).Value}/{match.Groups(7).Value} frames @ {match.Groups(10).Value}.{match.Groups(11).Value} fps, {match.Groups(14).Value,4} {match.Groups(16).Value}, {match.Groups(19).Value}.{match.Groups(20).Value} {match.Groups(21).Value} ({match.Groups(22).Value} {match.Groups(24).Value}), {match.Groups(17).Value} (-{match.Groups(18).Value})"
                 End If
             End If
         End If

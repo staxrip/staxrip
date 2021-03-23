a62db82dbd0018a31f8dda8e6a866933e3511adb
Dendraspis <dendraspis@gmx.net>
2021-03-23 10:40:16 +0100

    Apply new versioning to Assembly

--- Source/My Project/AssemblyInfo.vb
+++ Source/My Project/AssemblyInfo.vb
@@ -2,11 +2,11 @@ Imports System.Reflection
 Imports System.Runtime.InteropServices
 
 <Assembly: AssemblyTitle("StaxRip")>
-<Assembly: AssemblyCompany("StaxRip authors")>
+<Assembly: AssemblyCompany("StaxRip Authors")>
 <Assembly: AssemblyProduct("StaxRip")>
-<Assembly: AssemblyCopyright("Copyright (C) 2002-2021 StaxRip authors")>
+<Assembly: AssemblyCopyright("Copyright (C) 2002-2021 StaxRip Authors")>
 <Assembly: ComVisible(False)> 
 <Assembly: Guid("54d5dc0f-6ba7-42f0-a82f-9c71d561cb8b")>
-<Assembly: AssemblyVersion("2.1.9.0")>
-<Assembly: AssemblyFileVersion("2.1.9.0")>
+<Assembly: AssemblyVersion("2.1.15")>
+<Assembly: AssemblyFileVersion("2.1.15")>
 <Assembly: AssemblyDescription("Video encoding app")>

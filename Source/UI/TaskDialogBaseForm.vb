37f3360084362e26f2b37ef2cc919eb8a60fea6a
Dendraspis <dendraspis@gmx.net>
2021-03-25 18:40:07 +0100

    Fix TaskDialog wrongly drawn text

--- Source/UI/TaskDialogBaseForm.vb
+++ Source/UI/TaskDialogBaseForm.vb
@@ -260,14 +260,14 @@ Public Class TaskDialogBaseForm
             Dim r = New Rectangle(x, y, w, h)
 
             If Title <> "" AndAlso Description <> "" Then
-                TextRenderer.DrawText(g, Title, TitleFont, r, ForeColor, TextFormatFlags.VerticalCenter Or TextFormatFlags.Left)
+                TextRenderer.DrawText(g, Title, TitleFont, r, ForeColor, TextFormatFlags.Left Or TextFormatFlags.WordBreak)
                 y = CInt(titleFontHeight * 0.2) + GetTitleSize().Height
                 r = New Rectangle(x, y, w, h)
-                TextRenderer.DrawText(g, Description, Font, r, ForeColor, TextFormatFlags.VerticalCenter Or TextFormatFlags.Left)
+                TextRenderer.DrawText(g, Description, Font, r, ForeColor, TextFormatFlags.Left Or TextFormatFlags.WordBreak)
             ElseIf Title <> "" Then
-                TextRenderer.DrawText(g, Title, TitleFont, r, ForeColor, TextFormatFlags.VerticalCenter Or TextFormatFlags.Left)
+                TextRenderer.DrawText(g, Title, TitleFont, r, ForeColor, TextFormatFlags.Left Or TextFormatFlags.WordBreak)
             ElseIf Description <> "" Then
-                TextRenderer.DrawText(g, Description, Font, r, ForeColor, TextFormatFlags.VerticalCenter Or TextFormatFlags.Left)
+                TextRenderer.DrawText(g, Description, Font, r, ForeColor, TextFormatFlags.Left Or TextFormatFlags.WordBreak)
             End If
         End Sub
 

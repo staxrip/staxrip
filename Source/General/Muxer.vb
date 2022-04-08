3706f12ed6391f96e3b66668b072a36b2b044160
Valdiralita <valdiralita@yahoo.de>
2022-04-08 20:31:18 +0200

    fix linux problem when culture is empty

--- Source/General/Muxer.vb
+++ Source/General/Muxer.vb
@@ -677,7 +677,7 @@ Public Class MkvMuxer
             args += " --no-audio --no-subs --no-chapters --no-attachments --no-track-tags --no-global-tags"
         End If
 
-        If VideoTrackLanguage.ThreeLetterCode <> "und" Then
+        If VideoTrackLanguage.ThreeLetterCode IsNot Nothing And VideoTrackLanguage.ThreeLetterCode <> "und" Then
             args += " --language " & id & ":" + VideoTrackLanguage.ThreeLetterCode
         End If
 

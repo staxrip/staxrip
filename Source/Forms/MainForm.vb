257825b3be29746882f1b5f4d8f24dae5359a104
Dendraspis <dendraspis@gmx.net>
2023-05-21 10:23:43 +0200

    Fix Path.DirectorySeparatorChar in MainForm.vb

--- Source/Forms/MainForm.vb
+++ Source/Forms/MainForm.vb
@@ -1627,7 +1627,7 @@ Public Class MainForm
         Dim batchProject = ObjectHelp.GetCopy(Of Project)(p)
         batchProject.BatchMode = True
         batchProject.SourceFiles = {sourcefile}.ToList
-        Dim jobPath = Path.Combine(batchFolder, sourcefile.Dir.Replace("\", "-").Replace(":", "-") + " " + p.TemplateName + " - " + sourcefile.FileName + ".srip")
+        Dim jobPath = Path.Combine(batchFolder, sourcefile.Dir.Replace(Path.DirectorySeparatorChar, "-").Replace(":", "-") + " " + p.TemplateName + " - " + sourcefile.FileName + ".srip")
         SafeSerialization.Serialize(batchProject, jobPath)
         JobManager.AddJob(sourcefile.Base, jobPath)
     End Sub

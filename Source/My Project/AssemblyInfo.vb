084c39562234476c35b20cca90d9e93b2275c13a
Dendraspis <dendraspis@aikq.com>
2024-10-25 23:02:57 +0200

    v2.46.4

--- Source/My Project/AssemblyInfo.vb
+++ Source/My Project/AssemblyInfo.vb
@@ -7,6 +7,6 @@ Imports System.Runtime.InteropServices
 <Assembly: AssemblyCopyright("Copyright (C) 2002-2025 StaxRip Authors")>
 <Assembly: ComVisible(False)> 
 <Assembly: Guid("54d5dc0f-6ba7-42f0-a82f-9c71d561cb8b")>
-<Assembly: AssemblyVersion("2.46.3")>
-<Assembly: AssemblyFileVersion("2.46.3")>
+<Assembly: AssemblyVersion("2.46.4")>
+<Assembly: AssemblyFileVersion("2.46.4")>
 <Assembly: AssemblyDescription("Video encoding app")>

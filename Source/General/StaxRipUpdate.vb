3151c23c934d9899ccc377ba3b0b4f18f23718dd
Dendraspis <dendraspis@aikq.com>
2025-09-26 00:20:07 +0200

    Let Updater also search for Update-Releases

--- Source/General/StaxRipUpdate.vb
+++ Source/General/StaxRipUpdate.vb
@@ -57,82 +57,53 @@ Public Class StaxRipUpdate
         If Not (proceed OrElse force) Then Exit Sub
 
         Try
-            Dim changelogUrl = "https://raw.githubusercontent.com/staxrip/staxrip/master/CHANGELOG.md"
-            Dim releaseUrl = "https://github.com/staxrip/staxrip/releases"
+            'Dim changelogUrl = "https://raw.githubusercontent.com/staxrip/staxrip/master/CHANGELOG.md"
+            Const url = "https://api.github.com/repos/staxrip/staxrip/releases?per_page=5"
 
-            Dim currentVersion = Assembly.GetEntryAssembly.GetName.Version
-            Dim latestVersions = New List(Of (Version As Version, ReleaseType As String, SourceSite As String, DownloadUri As String, FileName As String))
-            Dim response = Await HttpClient.GetAsync(releaseUrl)
+            Dim currentVersion = Assembly.GetEntryAssembly().GetName().Version
+
+            HttpClient.DefaultRequestHeaders.Add("User-Agent", "Release-Checker")
+            Dim response = Await HttpClient.GetAsync(url)
             response.EnsureSuccessStatusCode()
             Dim content = Await response.Content.ReadAsStringAsync()
-            Dim linkMatches = Regex.Matches(content, "(?<="")/staxrip/staxrip/releases/tag/v?((\d+\.\d+\.\d+(?:\.\d+)?))(?="")")
+
+            Dim linkMatches = Regex.Matches(content, "(?<=""browser_download_url"":"")https://github\.com/staxrip/staxrip/releases/download/(?<tag>v\d\.\d+\.\d+(?:\.\d+)?)/StaxRip-v?(?<version>\d+\.\d+\.\d+(?:\.\d+)?)-x64(?<type>-.+?)?\.7z(?="")")
+            Dim latestVersions = New List(Of (Version As Version, ReleaseType As String, ReleaseUri As String, DownloadUri As String))
 
             For Each linkMatch As Match In linkMatches
-                Dim onlineVersionString = linkMatch.Groups(2).Value
+                Dim downloadUri = linkMatch.Groups(0).Value
+                Dim tag = linkMatch.Groups("tag").Value
+                Dim type = linkMatch.Groups("type").Value
+                Dim releaseType = If(type = "-UPDATE", "tool including update",
+                                    If(type = "-EXE", "hotfix/update", "release"))
+                Dim onlineVersionString = linkMatch.Groups("version").Value
                 Dim onlineVersion = Version.Parse(onlineVersionString)
+                Dim releaseUri = $"https://github.com/staxrip/staxrip/releases/tag/{tag}"
 
-                If onlineVersion <= currentVersion Then Exit For
+                If onlineVersion <= currentVersion OrElse (s.CheckForUpdatesDismissed <> "" AndAlso Version.Parse(s.CheckForUpdatesDismissed) >= onlineVersion) Then Continue For
 
-                Dim filename = $"StaxRip-v{onlineVersionString}-x64.7z"
-                Dim downloadUri = $"https://github.com/staxrip/staxrip/releases/download/v{onlineVersionString}/{filename}"
-                Dim releaseUri = $"https://github.com/staxrip/staxrip/releases/tag/v{onlineVersionString}"
-
-                latestVersions.Add((onlineVersion, "release", releaseUri, downloadUri, filename))
+                latestVersions.Add((onlineVersion, releaseType, releaseUri, downloadUri))
             Next
 
-            If latestVersions.Count > 0 Then
-                Dim latestVersion = latestVersions.OrderBy(Function(x) x.Version).Last()
+            If latestVersions.Any() Then
+                Dim sortedVersions = latestVersions.OrderByDescending(Function(x) x.Version).ToList()
+                Dim latestVersion = sortedVersions.First()
 
-                If latestVersion.Version > currentVersion AndAlso (s.CheckForUpdatesDismissed = "" OrElse
-                    Version.Parse(s.CheckForUpdatesDismissed) <> latestVersion.Version OrElse force) Then
+                Using td As New TaskDialog(Of String)
+                    td.Title = "A new " + latestVersion.ReleaseType + " was found: v" + latestVersion.Version.ToString()
+                    td.Icon = TaskIcon.Shield
 
-                    Using td As New TaskDialog(Of String)
-                        td.Title = "A new " + latestVersion.ReleaseType + " was found: v" + latestVersion.Version.ToString()
+                    td.AddCommand("Open release page", "open")
+                    td.AddCommand("Dismiss v" & latestVersion.Version.ToString(), "dismiss")
+                    td.AddCommand("Cancel", "cancel")
 
-                        Dim changelogResponse = Await HttpClient.GetAsync(changelogUrl)
-
-                        If changelogResponse.IsSuccessStatusCode Then
-                            Dim changelogContent = Await changelogResponse.Content.ReadAsStringAsync()
-                            Dim cleanedChangelogContent = Regex.Replace(changelogContent, "<!--[\s\S]*?-->", "").TrimStart()
-                            Dim splits = Regex.Split(cleanedChangelogContent, "\n\n\n")
-
-                            If splits.Any() Then
-                                Dim split = splits.Where(Function(x) x.Contains(latestVersion.Version.ToString()))?.LastOrDefault()
-
-                                If split <> "" Then
-                                    Dim changes = 0
-
-                                    td.Content += "Changes in this version:" + BR
-
-                                    For Each line In Regex.Split(split, "\n")
-                                        If changes >= 30 Then
-                                            td.Content += "..."
-                                            Exit For
-                                        ElseIf line.TrimStart().StartsWith("-") Then
-                                            line = Regex.Replace(line, "(?<=\W\(\[#\d+\])(\(/\.\./\.\./\w+/\d+\))(?=\)(?>,|\)|$))", "", RegexOptions.CultureInvariant)
-                                            line = Regex.Replace(line, "(?<=^| ) (?= |-)", "  ", RegexOptions.CultureInvariant)
-                                            td.Content += line + BR
-                                            changes += 1
-                                        End If
-                                    Next
-                                End If
-                            End If
-                        End If
-
-                        td.AddCommand("Open release page", "open")
-                        td.AddCommand("Dismiss v" & latestVersion.Version.ToString(), "dismiss")
-                        td.AddCommand("Cancel", "cancel")
-
-                        Select Case td.Show
-                            Case "open"
-                                g.ShellExecute(latestVersion.SourceSite)
-                            Case "dismiss"
-                                s.CheckForUpdatesDismissed = latestVersion.Version.ToString()
-                        End Select
-                    End Using
-                ElseIf force Then
-                    MsgInfo("No update available.")
-                End If
+                    Select Case td.Show()
+                        Case "open"
+                            g.ShellExecute(latestVersion.ReleaseUri)
+                        Case "dismiss"
+                            s.CheckForUpdatesDismissed = latestVersion.Version.ToString()
+                    End Select
+                End Using
             ElseIf force Then
                 MsgInfo("No update available.")
             End If
@@ -140,13 +111,5 @@ Public Class StaxRipUpdate
             s.CheckForUpdatesLastRequest = DateTime.Now
         Catch
         End Try
-    End Sub
-
-    Shared Sub OnDownloadComplete(sender As Object, e As AsyncCompletedEventArgs)
-        If Not e.Cancelled AndAlso e.Error Is Nothing Then
-            MsgInfo("Download succeeded!")
-        Else
-            MsgError("Download failed!")
-        End If
     End Sub
 End Class

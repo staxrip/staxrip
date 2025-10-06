917665e1c448f1e553075bb26729eb9f9b131193
Dendraspis <dendraspis@aikq.com>
2025-10-06 14:56:43 +0200

    UI Improvements

--- Source/UI/DataForm.vb
+++ Source/UI/DataForm.vb
@@ -1,9 +1,30 @@
 ﻿
+Imports StaxRip.UI
+
 Public Class DataForm
     Property HelpAction As Action
 
     Sub New()
         InitializeComponent()
+        ApplyTheme()
+
+        AddHandler ThemeManager.CurrentThemeChanged, AddressOf OnThemeChanged
+    End Sub
+
+    Sub OnThemeChanged(theme As Theme)
+        ApplyTheme(theme)
+    End Sub
+
+    Sub ApplyTheme()
+        ApplyTheme(ThemeManager.CurrentTheme)
+    End Sub
+
+    Sub ApplyTheme(theme As Theme)
+        If DesignHelp.IsDesignMode Then
+            Exit Sub
+        End If
+
+        BackColor = theme.General.BackColor
     End Sub
 
     Sub DataForm_HelpRequested(sender As Object, args As HelpEventArgs) Handles Me.HelpRequested

aab34a59c958d05ef96b618d135e99e8d29e8689
Dendraspis <dendraspis@aikq.com>
2025-12-25 15:14:35 +0100

    2026

--- Source/My Project/AssemblyInfo.vb
+++ Source/My Project/AssemblyInfo.vb
@@ -4,7 +4,7 @@ Imports System.Runtime.InteropServices
 <Assembly: AssemblyTitle("StaxRip")>
 <Assembly: AssemblyCompany("StaxRip Authors")>
 <Assembly: AssemblyProduct("StaxRip")>
-<Assembly: AssemblyCopyright("Copyright (C) 2002-2025 StaxRip Authors")>
+<Assembly: AssemblyCopyright("Copyright (C) 2002-2026 StaxRip Authors")>
 <Assembly: ComVisible(False)> 
 <Assembly: Guid("54d5dc0f-6ba7-42f0-a82f-9c71d561cb8b")>
 <Assembly: AssemblyVersion("2.50.5")>
